67a2cb8a732fa8275700b5b3913e1abdf8f598ba
DTSmth <ducktapedave85@gmail.com>
2026-02-25 06:47:28 -0500

    zipper linked list

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -13,17 +13,33 @@ class Node<T> {
     }
 }
 class Source {
-    public static <T> Node<T> reverseList(Node<T> head) {
-        return reverseList(head, null);
-    }
+    public static <T> Node<T> zipperLists(Node<T> head1, Node<T> head2) {
+        Node<T> head = head1;
+        Node<T> tail = head;
+        Node<T> current1 = head1.next;
+        Node<T> current2 = head2;
+        int count = 0;
 
-    public static <T> Node<T> reverseList(Node<T> head, Node<T> prev) {
-        if (head == null) {
-            return prev;
+        while (current1 != null && current2 != null) {
+            if (count % 2 == 0) {
+                tail.next = current2;
+                current2 = current2.next;
+            } else {
+                tail.next = current1;
+                current1 = current1.next;
+            }
+            tail = tail.next;
+            count += 1;
         }
-        Node<T> next = head.next;
-        head.next = prev;
-        return reverseList(next, head);
+
+        if (current1 != null) {
+            tail.next = current1;
+        }
+        if (current2 != null) {
+            tail.next = current2;
+        }
+
+        return head;
     }
 
 
