f18de20d78a87604fdc350330f9cdd32cfcc240a
DTSmth <ducktapedave85@gmail.com>
2026-02-26 07:17:30 -0500

    binary tree example

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -5,38 +5,29 @@ import java.util.ArrayList;
 
 class Node<T> {
     T val;
-    Node<T> next;
+    Node<T> left;
+    Node<T> right;
 
-    public Node(T val) {
+    Node(T val) {
         this.val = val;
-        this.next = null;
+        this.left = null;
+        this.right = null;
     }
 }
 class Source {
-    public static <T> Node<T> zipperLists(Node<T> head1, Node<T> head2) {
-
-        return zipperLists(head1, head2, 0);
-    }
-
-    public static <T> Node<T> zipperLists(Node<T> head1, Node<T> head2, int count) {
-        if (head1 == null) {
-            return head2;
-        }
-        if (head2 == null) {
-            return head1;
-        }
-        if (count % 2 == 0) {
-           head1.next = zipperLists(head1.next, head2, count += 1);
-           return head1;
-        } else {
-            head2.next = zipperLists(head1, head2.next, count += 1);
-            return head2;
-        }
-    }
-
     public static void run() {
         // this function behaves as `main()` for the 'run' command
         // you may sandbox in this function , but should not remove it
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+
+        a.left = b;
+        a.right = c;
+
+        System.out.println(a.left.val);
+
+
     }
 }
 
