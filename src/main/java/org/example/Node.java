5404ea0e8096cdabf95ab21af0d1cb1f497330f6
DTSmth <ducktapedave85@gmail.com>
2026-03-05 07:20:03 -0500

    Breadth First Traversal Binary Tree

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import java.util.List;
-import java.util.Stack;
 import java.util.ArrayList;
+import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 class Node<T> {
     T val;
@@ -17,21 +19,28 @@ class Node<T> {
 }
 
 class Source {
-    public static List<String> depthFirstValues(Node<String> root) {
+    public static List<String> breadthFirstValues(Node<String> root) {
         if (root == null) {
             return List.of();
         }
-        List<String> leftVals = depthFirstValues(root.left);
-        List<String> rightVals = depthFirstValues(root.right);
-
-        List<String> result = new ArrayList<>();
-        result.add(root.val);
-        result.addAll(leftVals);
-        result.addAll(rightVals);
-        return result;
+        Queue<Node<String>> queue = new ArrayDeque<>();
+        queue.add(root);
+        List<String> values = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            Node<String> node = queue.remove();
+            values.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return values;
     }
 
     public static void run() {
+
     }
 }
 
